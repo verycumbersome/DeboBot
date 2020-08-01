e0d5546017a1f9fe29f64508a4ac85cb673d5b06
Matthew Jordan <matthewsages@gmail.com>
2020-07-31 22:10:30 -0700

    update signature

--- engine.py
+++ engine.py
@@ -9,16 +9,29 @@ def main():
     consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
 
     ntp_time = utils.get_ntp_time()
-    url = "https://api.twitter.com/oauth/request_token"
-    headers = {
-            "oauth_consumer_key":consumer_key,
-            "oauth_nonce":"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg",
-            "oauth_signature":,
-            "oauth_signature_method":,
-            "oauth_timestamp":,
-            "oauth_token":,
-            "oauth_version":,
-            }
+    nonce = utils.get_nonce(32)
+
+    token = utils.get_token()
+    print(token)
+    # signature = utils.get_signature(
+            # True,
+            # consumer_key,
+            # nonce,
+            # "HMAC-SHA1",
+            # ntp_time,
+
+            # )
+
+    # url = "https://api.twitter.com/oauth/request_token"
+    # headers = {
+            # "oauth_consumer_key":consumer_key,
+            # "oauth_nonce":nonce,
+            # "oauth_signature":,
+            # "oauth_signature_method":,
+            # "oauth_timestamp":,
+            # "oauth_token":,
+            # "oauth_version":,
+            # }
     # timeline = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2")
     # print(timeline.headers)
 
--- utils.py
+++ utils.py
@@ -1,11 +1,18 @@
+import os
 import ntplib
+import urllib.parse
+import random
 
 from time import ctime
 from datetime import datetime
 from hashlib import sha1
 import hmac
 
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+
 def get_ntp_time():
+    """Return the NTP time diff in seconds"""
     c = ntplib.NTPClient()
     response = c.request('europe.pool.ntp.org', version=3)
     ntp_time = round(
@@ -17,8 +24,31 @@ def get_ntp_time():
 
     return ntp_time
 
-def get_signature():
+def get_nonce(length=8):
+    """Generate pseudorandom number"""
+    return ''.join([str(random.randint(0, 9)) for i in range(length)])
+
+def get_token():
+    oauth_callback = "https%3A%2F%2Ftwitter.com"
+    oauth_signature_method = "HMAC-SHA1"
+    oauth_timestamp = get_ntp_time()
+    oauth_consumer_key = consumer_key
+    # oauth_signature = get_signature
+    oauth_version = "1.0"
+
+    get_signature(oauth_consumer_key, "")
+    print(oauth_consumer_key)
+
+
+def get_signature(consumer_secret, token_secret):
+    """Create API request signature"""
     # key = b"CONSUMER_SECRET&" #If you dont have a token yet
+    consumer_secret + "&"
+    consumer_secret.encode('utf-8')
+    token_secret.encode('utf-8')
+    key = consumer_secret + token_secret
+    print(key)
+
     key = b"CONSUMER_SECRET&TOKEN_SECRET"
 
 
