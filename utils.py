aca3f922877e774b630c07006a1d20b48996bf88
Matthew Jordan <matthewsages@gmail.com>
2020-08-02 01:34:07 -0700

    Complete step one of Oauth process

--- utils.py
+++ utils.py
@@ -2,7 +2,11 @@ import os
 import ntplib
 import urllib.parse
 import random
+import base64
+import requests
 
+from requests_oauthlib import OAuth1
+from collections import OrderedDict
 from time import ctime
 from datetime import datetime
 from hashlib import sha1
@@ -10,6 +14,14 @@ import hmac
 
 consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
 consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+
+def percent_encoding(string):
+    """Percent encode a string"""
+    result = ''
+    accepted = [c for c in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~'.encode('utf-8')]
+    for char in string.encode('utf-8'):
+        result += chr(char) if char in accepted else '%{}'.format(hex(char)[2:]).upper()
+    return result
 
 def get_ntp_time():
     """Return the NTP time diff in seconds"""
@@ -29,29 +41,51 @@ def get_nonce(length=8):
     return ''.join([str(random.randint(0, 9)) for i in range(length)])
 
 def get_token():
-    oauth_callback = "https%3A%2F%2Ftwitter.com"
-    oauth_signature_method = "HMAC-SHA1"
-    oauth_timestamp = get_ntp_time()
-    oauth_consumer_key = consumer_key
-    # oauth_signature = get_signature
-    oauth_version = "1.0"
+    """Get OAuth Access token"""
 
-    get_signature(oauth_consumer_key, "")
+    url = "https://api.twitter.com/oauth/request_token"
+    parameters = {
+        "oauth_consumer_key":consumer_key,
+        "oauth_nonce":get_nonce(),
+        "oauth_callback":"https%3A%2F%2Ftwitter.com",
+        "oauth_signature_method":"HMAC-SHA1",
+        "oauth_timestamp":get_ntp_time(),
+        "oauth_version":"1.0",
+        }
+
+    parameters["oauth_signature"] = get_signature(
+            "POST",
+            url,
+            parameters["oauth_consumer_key"],
+            "",
+            parameters
+            ).decode("utf-8")
+
+    dst = "Oauth"
+
+    for item in parameters.items():
+        dst += " " + percent_encoding(item[0]) + "=\"" + percent_encoding(str(item[1])) + "\","
+
+    dst = dst[:-1]
+    print(dst)
+    # requests.get(url, auth=auth)
 
 
-def get_signature(consumer_secret, token_secret):
+def get_signature(method, url, consumer_secret, token_secret, parameters):
     """Create API request signature"""
+    output = method + "&" + percent_encoding(url)
+
+    # Sorted parameters for signature
+    sorted_params = {k: percent_encoding(str(parameters[k])) for k in sorted(parameters)}
+
+    # Generate signature base string
+    for item in sorted_params.items():
+        output += "&" + item[0] + "=" + str(item[1])
+
     # key = b"CONSUMER_SECRET&" #If you dont have a token yet
     key = (consumer_secret + "&" + token_secret).encode()
-    print(key)
 
-    # key = b"CONSUMER_SECRET&TOKEN_SECRET"
-
-
-    # The Base String as specified here:
-    raw = b"BASE_STRING" # as specified by OAuth
-
-    hashed = hmac.new(key, raw, sha1)
+    hashed = hmac.new(key, output.encode(), sha1)
 
     # The signature
-    return hashed.digest().encode("base64").rstrip('\n')
+    return base64.b64encode(hashed.digest())
