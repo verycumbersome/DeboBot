0be35661bd5c8b3dd7320f6e9ec314de21685f97
Matthew Jordan <matthewsages@gmail.com>
2020-08-05 08:38:04 -0700

    Fix formatting

--- engine.py
+++ engine.py
@@ -1,39 +1,20 @@
-import os
-import requests
-
-import engine
 import utils
 
 def main():
-    consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
-    consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
-
+    """Main function loop"""
     # ntp_time = utils.get_ntp_time()
     # nonce = utils.get_nonce(32)
 
     # token = utils.get_token()
-    timeline = utils.get_timeline("twitterapi", 2)
-    # signature = utils.get_signature(
-            # True,
-            # consumer_key,
-            # nonce,
-            # "HMAC-SHA1",
-            # ntp_time,
 
-            # )
+    method = "GET"
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    parameters = {
+        "screen_name":"killmefam",
+        "count":"2",
+        }
+    timeline = utils.make_call(method, url, parameters)
+    print(timeline.content)
 
-    # url = "https://api.twitter.com/oauth/request_token"
-    # headers = {
-            # "oauth_consumer_key":consumer_key,
-            # "oauth_nonce":nonce,
-            # "oauth_signature":,
-            # "oauth_signature_method":,
-            # "oauth_timestamp":,
-            # "oauth_token":,
-            # "oauth_version":,
-            # }
-    # timeline = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2")
-    # print(timeline.headers)
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,22 +1,19 @@
+"""Utils for Twitter API"""
 import os
-import ntplib
-import urllib.parse
-import random
 import base64
-import requests
+import random
 import logging
-
-from requests_oauthlib import OAuth1
-from collections import OrderedDict
-from time import ctime
+import hmac
 from datetime import datetime
 from hashlib import sha1
-import hmac
+from time import ctime
+import requests
+import ntplib
 
 consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
 consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
-oauth_token = "350639912-jwot3WoXlJFoxosRYZbVSGjhM5xEbGOfTFBenE0P"
-oauth_token_secret = "dMsRtqFFwXZGwRbyiO8TddPcx04YU0Uc6bs2B5zGCFsrb"
+oauth_token = os.environ.get("TWITTER_TOKEN")
+oauth_token_secret = os.environ.get("TWITTER_TOKEN_SECRET")
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.ERROR)
@@ -38,11 +35,11 @@ def get_ntp_time():
     c = ntplib.NTPClient()
     response = c.request('europe.pool.ntp.org', version=3)
     ntp_time = round(
-            (datetime.now() - datetime.strptime(
-                ctime(response.offset),
-                "%a %b %d %H:%M:%S %Y")
-                ).total_seconds()
-            )
+        (datetime.now() - datetime.strptime(
+            ctime(response.offset),
+            "%a %b %d %H:%M:%S %Y")
+        ).total_seconds()
+    )
 
     return ntp_time
 
@@ -52,11 +49,10 @@ def get_nonce(length=32):
     randnum = random.randint(10 ** (length - 1), (10 ** length) - 1)
     data = base64.b64encode(str(randnum).encode())
 
-    return (data[:length] + (data[length:] and b""))
+    return data[:length] + (data[length:] and b"")
 
-def get_timeline(user, count):
-    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-
+def make_call(method, url, parameters):
+    """Make a call to the Twitter API"""
     auth = {
         "oauth_consumer_key":consumer_key,
         "oauth_nonce":get_nonce(32).decode(),
@@ -65,18 +61,13 @@ def get_timeline(user, count):
         "oauth_token":oauth_token,
         "oauth_version":"1.0",
         }
-    parameters = {
-        "screen_name":user,
-        "count":str(count),
-        }
 
     auth["oauth_signature"] = get_signature(
-            "GET",
-            url,
-            consumer_secret,
-            oauth_token_secret,
-            {**parameters, **auth}
-            ).decode("utf-8")
+        method,
+        url,
+        oauth_token_secret,
+        {**parameters, **auth}
+        ).decode("utf-8")
 
     auth = {k: auth[k] for k in sorted(auth)}
 
@@ -86,16 +77,13 @@ def get_timeline(user, count):
         dst += " " + percent_encoding(item[0]) + "=\"" + percent_encoding(str(item[1])) + "\","
     dst = dst[:-1]
 
-    timeline = requests.get(
-            url,
-            params=parameters,
-            headers={'Authorization': dst}
-            )
+    return requests.get(
+        url,
+        params=parameters,
+        headers={'Authorization': dst}
+        )
 
-    print(timeline.content)
-
-
-def get_signature(method, url, consumer_secret, token_secret, parameters):
+def get_signature(method, url, token_secret, parameters):
     """Create API request signature"""
     # output = method + "&" + percent_encoding(url)
     parameter_string = ""
@@ -107,10 +95,8 @@ def get_signature(method, url, consumer_secret, token_secret, parameters):
     for item in sorted_params.items():
         parameter_string += "&" + percent_encoding(item[0]) + "=" + percent_encoding(str(item[1]))
 
-    # print(parameter_string)
     output = method + "&" + percent_encoding(url) + "&" + percent_encoding(parameter_string[1:])
 
-    # print(output, "\n\n\n")
     # key = b"CONSUMER_SECRET&" #If you dont have a token yet
     key = (percent_encoding(consumer_secret) + "&" + percent_encoding(token_secret)).encode()
 
