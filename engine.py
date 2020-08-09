d827dd68a7d0a5178046a81799fcea44f21682e2
Matthew Jordan <matthewsages@gmail.com>
2020-08-09 11:25:32 -0700

    Add model

--- engine.py
+++ engine.py
@@ -25,21 +25,25 @@ def get_timeline(screen_name, depth):
     print("Scraping tweets from \'" + screen_name + "\'")
     for i in tqdm.tqdm(range(depth)):
         parameters = {
-            "max_id":max_id - 1,
-            "screen_name":screen_name,
-            "include_rts":"false",
-            "exclude_replies":"true",
-            "tweet_mode":"extended",
-            "count":str(config.SCRAPE_TIMELINE_COUNT),
-            }
+                "max_id":max_id - 1,
+                "screen_name":screen_name,
+                "include_rts":"false",
+                "exclude_replies":"true",
+                "tweet_mode":"extended",
+                "count":str(config.SCRAPE_TIMELINE_COUNT),
+                }
         if not max_id: parameters.pop("max_id")
 
         timeline = utils.make_call(method, url, parameters)
 
-        for item in json.loads(timeline.content):
-            max_id = item["id"]
-            text = "<|startoftext|>" + re.sub(r"http\S+", "", item["full_text"]) + "<|endoftext|>"
-            data.append(text)
+        if (timeline.status_code == 200):
+            for item in json.loads(timeline.content):
+                max_id = item["id"]
+                text = re.sub(r"http\S+", "", item["full_text"]) + "<eos>"
+                data.append(text)
+
+        else:
+            print(timeline.content, end="\r")
 
     return data
 
@@ -58,55 +62,19 @@ def main():
         utils.convert_to_txt("data/textdata.csv")
 
     if "--gen" in sys.argv:
-        # model = GPT2Model(config.MODEL_CONFIG)
-        # model.load_state_dict(torch.load(config.MODEL_PATH, map_location=torch.device('cpu')))
-
-        model_name = "124M"
-        if not os.path.isdir(os.path.join("models", model_name)):
-            print(f"Downloading {model_name} model...")
-            gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
-
-        file_name = "data/textdata.txt"
         sess = gpt2.start_tf_sess()
-        gpt2.finetune(sess,
-                      file_name,
-                      model_name=model_name,
-                      steps=1000)   # steps is max number of training steps
+        gpt2.load_gpt2(sess, run_name='run1')
 
-        gpt2.generate(sess)
+        while(True):
+            text = gpt2.generate(sess,
+                    length=250,
+                    temperature=0.7,
+                    prefix=input("Enter prefix:"),
+                    nsamples=1,
+                    batch_size=1
+                    )
 
-
-
-        # model = torch.load(config.MODEL_PATH, map_location=torch.device('cpu'))
-        # print(model)
-
-        # review_text = "I love completing my todos! Best app ever!!!"
-        # while(True):
-        # review_text = input("Enter Tweet:")
-        # encoded_review = config.TOKENIZER.encode_plus(
-                # text=review_text,
-                # truncation=True,
-                # max_length=config.MAX_LEN,
-                # add_special_tokens=True,
-                # return_token_type_ids=False,
-                # pad_to_max_length=True,
-                # return_attention_mask=True,
-                # return_tensors='pt',
-                # )
-
-        # print(encoded_review)
-
-        # input_ids = encoded_review['input_ids'].to(device)
-        # attention_mask = encoded_review['attention_mask'].to(device)
-        # output = model(
-                # input_ids=input_ids,
-                # attention_mask=attention_mask
-                # )
-        # _, prediction = torch.max(output, dim=1)
-        # print(prediction)
-        # print(f'Review text: {review_text}')
-        # print(f'Sentiment  : {class_names[prediction]}')
-
+            print(text)
 
 if __name__ == "__main__":
     main()
