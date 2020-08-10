0b0a26f91d13cdf781057178fdca1ad5d3fc2556
Matthew Jordan <matthewsages@gmail.com>
2020-08-09 18:47:33 -0700

    Add flask

--- src/engine.py
+++ src/engine.py
@@ -46,45 +46,55 @@ def get_timeline(screen_name, depth):
 
     return data
 
+def error(msg):
+    print("ERROR:", msg)
+    exit()
 
 def main():
     """Main function"""
     for index, arg in enumerate(sys.argv):
         if arg == "--scrape":
-            data = {"text": []}
+            try:
+                data = {"text": []}
 
-            for screen_name in config.TWITTER_NAMES:
-                data["text"].extend(get_timeline(
-                    screen_name, config.SCRAPE_DEPTH))
+                for screen_name in config.TWITTER_NAMES:
+                    data["text"].extend(get_timeline(
+                        screen_name, config.SCRAPE_DEPTH))
 
-            df = pd.DataFrame(data=data)
-            df.to_csv("data/textdata.csv", index=False)
+                df = pd.DataFrame(data=data)
+                df.to_csv("data/textdata.csv", index=False)
+
+            except:
+                error("Must enter scrape depth and length")
 
         if arg == "--txt":
             utils.convert_to_txt("data/textdata.csv")
 
-        if arg == "--user":
-            print(sys.argv[index + 1])
-
         if arg == "--gen":
-            print(sys.argv[index + 1])
             sess = gpt2.start_tf_sess()
             gpt2.load_gpt2(sess, run_name='run1')
 
-            while(True):
-                text = gpt2.generate(
-                        sess,
-                        length=250,
-                        temperature=0.85,
-                        include_prefix=False,
-                        # prefix=input("Enter prefix:"),
-                        truncate='<|endoftext|>',
-                        nsamples=5,
-                        batch_size=5
-                        )
+            gen_file = 'gpt2_gentext_{:%Y%m%d_%H%M%S}.txt'.format(datetime.utcnow())
 
-                print(text)
+            gpt2.generate_to_file(sess,
+                      destination_path=gen_file,
+                      length=500,
+                      temperature=0.7,
+                      nsamples=100,
+                      batch_size=20
+                      )
 
+            # text = gpt2.generate(
+                    # sess,
+                    # length=250,
+                    # temperature=0.7,
+                    # prefix = "<|startoftext|>",
+                    # include_prefix=False,
+                    # # prefix=input("Enter prefix:"),
+                    # truncate='<|endoftext|>',
+                    # nsamples=1000,
+                    # batch_size=5
+                    # )
 
 if __name__ == "__main__":
     main()
