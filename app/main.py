e1cd1ab43eba589ca66135ba29336142ee872574
Matthew Jordan <matthewsages@gmail.com>
2020-08-10 01:12:19 -0700

    Update text

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import random
 import pandas as pd
 from flask import Flask
@@ -8,17 +9,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    with open("../gpt2_gentext_20200810_015757.txt", "r") as file:
-        tweets = file.read().split("<|startoftext|>")
-        tweets = list(set([tweet.replace("<|endoftext|>", "") for tweet in tweets]))
+    tweets = []
+    text_path = "../text/"
+    for file in os.listdir(text_path):
+        with open(text_path + file, "r") as fp:
+            tweets.extend(fp.read().split("<|startoftext|>"))
+    # Remove duplicates and exd of text
+    tweets = list(set([tweet.replace("<|endoftext|>", "") for tweet in tweets]))
+    print(len(tweets))
 
+    #Select a random tweet from the options
     tweet = random.choice(tweets)
 
     training = pd.read_csv("../src/data/textdata.csv")
 
-    while ([True for item in training["text"] if tweet.lower() == item.lower()]):
+    while ([True for item in training["text"] if str(tweet).lower() == str(item).lower()]):
         print("UNORIGINAL TWEET:", tweet)
         tweet = random.choice(tweets)
+
+    print(tweet)
 
     return "<h1>" + tweet + "</h1>"
 
